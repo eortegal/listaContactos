50959fd33192dd24c6e934da5785ae6aee5c604b
EOL <erickol8@hotmail.com>
2020-05-19 15:03:46 -0500

    Initial Commit

---
+++ listaContactos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  listaContactos
+//
+//  Created by Mmartz on 19/05/20.
+//  Copyright © 2020 ctemdn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
